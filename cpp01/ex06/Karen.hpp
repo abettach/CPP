96c4556eb4e5de3ee9e2e45d0d2496063f5417e5
root <bettachiachraf@gmail.com>
2021-08-11 13:46:37 +0100

    done

---
+++ cpp01/ex06/Karen.hpp
@@ -0,0 +1,23 @@
+#ifndef KAREN_CPP
+#define KAREN_CPP
+
+#include <iostream>
+
+/*Declaring a pointer to a member function:
+return_type (class_name::*pointer_name) (parameter types)
+*/
+class Karen
+{
+	private:
+    	void debug( void );
+		void info( void );
+		void warning( void );
+		void error( void );
+	public:
+		Karen();
+		~Karen();
+		void	(Karen::*pointer[4])(void);
+		void complain( std::string level );
+};
+
+#endif
