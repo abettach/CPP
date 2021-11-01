e96ad0ae8a365bc4c41eecd94a84df23ebfe5c71
abettach <bettachiachraf@gmail.com>
2021-11-01 23:26:43 +0100

    cpp6

--- cpp06/ex00/conversion.cpp
+++ cpp06/ex00/conversion.cpp
@@ -1,133 +1,256 @@
 #include "conversion.hpp"
-#include <string>
 
-conversion::conversion(/* args */)
+Type::Type()
 {
-    _point = false;
+	type = "";
+	impossible = false;
+	non_displayable = false;
 }
 
-bool     conversion::toInt(std::string str)
+bool maxInt(int nbr, std::string s2)
 {
-    if (inf_check(str))
-    {
-        std::cout <<"Impossible";
-        return false;
-    }
-    if (str == ONLY_POINT)
-    {
-        this->_int_num = 0;
-        return true;
-    }
-    if (ischar(str))
-    {
-        this->_int_num = str[0];
-    }
-    else
-        this->_int_num = atoi(str.c_str());
-    return (true);
+	std::string s1;
+	size_t i = 0;
+	size_t i2 = 0;
+
+	s1 = std::to_string(nbr);
+	while (s2[i2] == '0')
+		i2++;
+	
+	while (i < s2.length())
+	{
+		if (s2[i2] == '.')
+			break;
+		if (s1[i] != s2[i2])
+			return (false);
+		i++;
+		i2++;
+	}
+	// for (size_t i = 0; (i < s2.length() - 1); i++)
+	// {
+	// 	if (s2[i] == '.')
+	// 		break;
+	// 	if (s1[i] != s2[i])
+	// 		return (false);
+	// }
+	return (true);
 }
 
-bool     conversion::toFloat(std::string str)
+bool isValid(std::string str, size_t start)
 {
-    if (inf_check(str))
-        return true;
-    if (str == ONLY_POINT)
-    {
-        this->_float_num = 0.0f;
-        return true;
-    }
-    if (ischar(str))
-        this->_float_num = this->_int_num;
-    else
-        this->_float_num = atof(str.c_str());
-    return (true);
+
+	for (; start < str.length() - 1; start++)
+	{
+		if (!std::isdigit(str[start]) && str[start] != '.')
+		{
+			return (false);
+		}
+	}
+	return (true);
 }
 
-bool     conversion::toDouble(std::string str)
+void Type::setValue(std::string str)
 {
-    if (inf_check(str))
-        return true;
-     if (str == ONLY_POINT)
-    {
-        this->_double_num = 0.0;
-        return true;
-    }
-    if (ischar(str))
-        this->_double_num = this->_float_num;
-    else
-        this->_double_num = atof(str.c_str());
-    return (true);
+	value = str;
 }
 
-bool     conversion::toChar(std::string str)
+std::string Type::getValue() const
 {
-    if (inf_check(str))
-    {
-        std::cout <<"Impossible";
-        return false;
-    }
-    if (str == ONLY_POINT)
-    {
-        this->_char_num = 46;
-        return true;
-    }
-    toInt(str);
-    this->_char_num = this->_int_num;
-    if (this->_char_num < 32 || this->_char_num >= 127)
-        return (false);
-    return (true);
+	return value;
 }
 
-int    conversion::inf_check(std::string str)
+int isPoint(std::string str, size_t start)
 {
-    if (str == "nan" || str == "nanf" || str == "-inff"
-        || str == "+inff" || str == "-inf" || str == "+inf")
-    {
-        if (str == "nan" ||  str == "-inf" || str == "+inf")
-            return 1;
-        if (str == "nanf" || str == "-inff" || str == "+inff")
-            return 2;
-    }
-    return 0;
+	bool point;
+
+	point = false;
+	for (; start < str.length() - 1; start++)
+	{
+		if ((point && str[start] == '.'))
+		{
+			return 2;
+		}
+		if (str[start] == '.')
+			point = true;
+	}
+	if (point ^ (str[str.length() - 1] == '.'))
+		return 1;
+	return 0;
 }
 
-void    conversion::ft_print(std::string str)
+void Type::parsing(std::string &Type)
 {
-    size_t i = 0;
+	size_t Tsize;
+	size_t i;
 
-    if (isdigit(str))
-    {
-        if (str == ".f")
-            str = "0.0f";
-        else if (str[0] == '.' && str[1])
-            str = "0" + str;
-        while (str[i] != '.' && str[i])
-            i++;
-        if (str[i] == '.')
-            _point = true;
-        {   
-            std::cout << "Char : ";
-            char_print(str);
-            std::cout << std::endl;
-        }
-        {
-            std::cout << "int : ";
-            int_print(str);
-            std::cout << std::endl;
-        }
-        {
-            std::cout << "float : ";
-            float_print(str);
-            std::cout << std::endl;
-        }
-        {
-            std::cout << "Double : ";
-            double_print(str);
-            std::cout << std::endl;
-        }
-    }
-    else
-        std::cout << "Error: Paramettre not Digite" << std::endl;
+	Tsize = Type.length();
+	i = 0;
+	if (Tsize == 1 && !std::isdigit(Type[i]))
+	{
+		this->type = "char";
+		return;
+	}
+	else if (Type == "+inff" || Type == "-inff" || Type == "nanf")
+	{
+		this->type = "float";
+		impossible = true;
+		return;
+	}
+	else if (Type == "+inf" || Type == "-inf" || Type == "nan")
+	{
+		this->type = "double";
+		impossible = true;
+		return;
+	}
+	if (Type[i] == '+' || Type[i] == '-')
+		i++;
+	this->type = "int";
+	if ((isPoint(Type, i) == 1) && isValid(Type, i) && Type[Tsize - 1] == 'f')
+		this->type = "float";
+	else if (((isPoint(Type, i) == 1) && isValid(Type, i)) && (std::isdigit(Type[Tsize - 1]) || (Type[Tsize - 1] == '.')))
+		this->type = "double";
+	else if ((isPoint(Type, i) == 2) || !(isValid(Type, i)) || (!std::isdigit(Type[Tsize - 1])))
+		this->type = "Invalid Type";
+	return;
 }
 
-conversion::~conversion(){}
+void Type::SetTypes()
+{
+	std::string str = getValue();
+	if (this->type == "char")
+	{
+		char_value = str[0];
+		int_value = static_cast<int>(char_value);
+		float_value = static_cast<float>(char_value);
+		double_value = static_cast<double>(char_value);
+		if (!std::isprint(int_value))
+			non_displayable = true;
+	}
+	else if (this->type == "int")
+	{
+		int_value = std::atoi(str.c_str());
+		if (!maxInt(int_value, value))
+			impossible = true;
+		char_value = static_cast<char>(int_value);
+		float_value = static_cast<float>(int_value);
+		double_value = static_cast<double>(int_value);
+
+		if (impossible)
+		{
+			float_value = static_cast<float>(std::atof(str.c_str()));
+			double_value = static_cast<double>(std::atof(str.c_str()));
+		}
+		if (!std::isprint(int_value))
+			non_displayable = true;
+	}
+	else if (this->type == "float")
+	{
+		float_value = std::atof(str.c_str());
+		if (!maxInt(float_value, value))
+			impossible = true;
+		int_value = static_cast<int>(float_value);
+
+		double_value = static_cast<double>(float_value);
+		char_value = static_cast<char>(float_value);
+
+		if (!std::isprint(int_value))
+			non_displayable = true;
+	}
+	else if (this->type == "double")
+	{
+		double_value = atof(str.c_str());
+		if (!maxInt(double_value, value))
+			impossible = true;
+		float_value = static_cast<float>(double_value);
+		int_value = static_cast<int>(double_value);
+		char_value = static_cast<char>(double_value);
+		if (!std::isprint(int_value))
+			non_displayable = true;
+	}
+}
+
+void Type::princhar_value()
+{
+	if (impossible)
+		std::cout << "char: impossible" << std::endl;
+	else if (non_displayable)
+		std::cout << "char: Non displayable" << std::endl;
+	else
+		std::cout << "char: '" << char_value << "'" << std::endl;
+}
+
+void Type::prinint_value()
+{
+	if (impossible)
+		std::cout << "int: impossible" << std::endl;
+	else
+		std::cout << "int: " << int_value << std::endl;
+	impossible = false;
+}
+
+void Type::prinfloat_value()
+{
+
+	std::cout << "float: " << std::fixed << std::setprecision(precesion) << float_value << 'f' << std::endl;
+}
+
+void Type::prindouble_value()
+{
+
+	std::cout << "double: " << std::fixed << std::setprecision(precesion) << double_value << std::endl;
+}
+
+void Type::setPrecision(void)
+{
+	if (type == "double" || type == "float")
+	{
+		this->precesion = value.find(".");
+		this->precesion = (value.length() - this->precesion - 1);
+		if (type == "float" && !(value.find(".") == value.find("f") - 1))
+			this->precesion--;
+		if (value.find(".") == value.length() - 1)
+			this->precesion++;
+	}
+	else
+		this->precesion = 1;
+}
+
+void Type::printTypes()
+{
+	setPrecision();
+	princhar_value();
+	prinint_value();
+	prinfloat_value();
+	prindouble_value();
+}
+
+std::string Type::GetType() const
+{
+	return (this->type);
+}
+
+Type::Type(const Type &other)
+{
+	*this = other;
+}
+
+Type &Type::operator=(const Type &rhs)
+{
+	if (this != &rhs)
+	{
+		this->int_value = rhs.int_value;
+		this->char_value = rhs.char_value;
+		this->float_value = rhs.float_value;
+		this->double_value = rhs.double_value;
+		this->impossible = rhs.impossible;
+		this->non_displayable = rhs.non_displayable;
+		this->type = rhs.type;
+		this->value = rhs.value;
+		this->precesion = rhs.precesion;
+	}
+	return (*this);
+}
+
+Type::~Type()
+{
+}
--- cpp06/ex00/conversion.hpp
+++ cpp06/ex00/conversion.hpp
@@ -1,36 +1,40 @@
-#ifndef CONVERTION_HPP
-#define CONVERTION_HPP
-#include <iostream>
-#define FALSE_POINT_F ".0f"
-#define TRUE_POINT_F "f"
-#define ONLY_POINT "."
-class conversion
-{
-private:
-    std::string _toInt;
-    std::string _toFloat;
-    std::string _toChat;
-    int _int_num;
-    float _float_num;
-    double _double_num;
-    char    _char_num;
-    bool _point;
-public:
-    conversion(/* args */);
-    ~conversion();
-    bool toInt(std::string target);
-    bool toFloat(std::string target);
-    bool toDouble(std::string target);
-    bool toChar(std::string target);
-    void    ft_print(std::string str);
-    void    char_print(std::string str);
-    void    int_print(std::string str);
-    void    double_print(std::string str);
-    void    float_print(std::string str);
-    int    inf_check(std::string str);
-    bool    isdigit(std::string str);
-    bool    ischar(std::string str);
-};
+#ifndef __CONVERSION_HPP__
+# define __CONVERSION_HPP__
 
+# include <iostream>
+# include <cstdlib>
+#include <iomanip>
+
+
+class Type
+{
+private :
+	int 		int_value;
+	char		char_value;
+	float 		float_value;
+	double		double_value;
+	bool		impossible;
+	bool		non_displayable;
+	std::string type;
+	std::string	value;
+	int			precesion;
+public :
+	std::string GetType() const;
+	std::string getValue() const;
+	Type();
+	void	setValue(std::string );
+	void	setPrecision( void );
+	void	parsing(std::string &Type);
+	void	SetTypes( void );
+	void	printTypes( void )	;
+	void	princhar_value( void )		;
+	void	prinint_value( void )		;
+	void	prinfloat_value( void )	;
+	void	prindouble_value( void )	;
+
+	Type(const Type &);
+	Type &operator=(const Type &);
+	~Type();
+};
 
 #endif
